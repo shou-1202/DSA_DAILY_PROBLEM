219a63edf1a4e73cf32fb193881ad010432cb7e3
shou-1202 <shouryagarg1202@gmail.com>
2026-06-23 08:57:07 +0530

    Add files via upload

---
+++ 455. Assign Cookies/Solution.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int findContentChildren(vector<int>& g, vector<int>& s) {
+        if(s.size()==0)return 0;
+        sort(g.begin(), g.end());
+        sort(s.begin(), s.end());
+
+        int i = 0, j = 0;
+        int n = g.size(), m = s.size();
+        int ans = 0;
+        while(i<n && j<m){
+            if(g[i] <= s[j]){
+                ans++;
+                i++;j++;
+            }
+            else{
+                j++;
+            }
+        }
+        return ans;
+    }
+};
